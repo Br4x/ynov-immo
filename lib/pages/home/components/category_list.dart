ab4c4951f1d794ccb30b68b6f8725b20f22f7798
souleiseg <soulei.seg@gmail.com>
2021-02-04 19:38:06 +0100

    Icones + ajustement boutons

--- lib/pages/home/components/category_list.dart
+++ lib/pages/home/components/category_list.dart
@@ -8,30 +8,11 @@ class CategoryList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          CategoryItem(
-            title: "Combo Meal",
-            isActive: true,
-            press: () {},
-          ),
-          CategoryItem(
-            title: "Chicken",
-            press: () {},
-          ),
-          CategoryItem(
-            title: "Beverages",
-            press: () {},
-          ),
-          CategoryItem(
-            title: "Snacks & Sides",
-            press: () {},
-          ),
-        ],
-      ),
-    );
+  return Center(
+      child: Padding(
+      padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+      child: Text('Vous souhaitez :',
+      style: TextStyle(fontSize: 22))
+  ));
   }
 }
