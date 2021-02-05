9ca23939ffc8e396b729eb587f3b2e079ff6b777
totiii <tpommier562@gmail.com>
2021-02-05 15:04:37 +0100

    page display with data

--- lib/pages/detailMaison/components/body.dart
+++ lib/pages/detailMaison/components/body.dart
@@ -63,109 +63,191 @@ class ItemInfo extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       padding: EdgeInsets.all(20),
-      child: Column(
-        children: <Widget>[
-
-          CarouselSlider(
-            options: CarouselOptions(
-                height: 250.0,
-                autoPlay: true,
-                aspectRatio: 2.0,
-                enlargeCenterPage: true,
-            ),
-            items: [1,2,3,4,5].map((i) { //todo url api route /real-estate-image
-              return Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      decoration: BoxDecoration(
-                          color: Colors.transparent
-                      ),
-                      child: Image.network('https://picsum.photos/250?image=9')
-                  );
-                },
-              );
-            }).toList(),
-          ),
-          Row( //todo info /real-estate
+      child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: new Column(
               children: <Widget>[
-                Text(
-                  realEstateData.address,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.black),
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: 250.0,
+                    autoPlay: true,
+                    aspectRatio: 2.0,
+                    enlargeCenterPage: true,
+                  ),
+                  items: [1,2,3,4,5].map((i) { //todo url api route /real-estate-image
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: BoxDecoration(
+                                color: Colors.transparent
+                            ),
+                            child: Image.network('https://picsum.photos/250?image=9')
+                        );
+                        },
+                    );
+                  }).toList(),
                 ),
 
-                SizedBox(width: 20),
+                Container (
+                  padding: const EdgeInsets.all(3.0),
+                  child: new Row (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      GestureDetector(
+                          child: new Text(realEstateData.type,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.white),
+                          )
+                      ),
 
-                Text(
-                  '18000€',
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.red),
+                      SizedBox(width: 20),
+
+                      GestureDetector(
+                          child: new Text(realEstateData.price.toString() + " €",
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.red),
+                          )
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-          ),
 
-          Row(
-            children: <Widget>[
-              Text(
-                'Critères :',
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.black),
-              ),
-            ]),
+                Container (
+                  padding: const EdgeInsets.all(3.0),
+                  child: new Row (
+                    children: <Widget>[
+                      Text(
+                        'Critères :',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
 
-            Row(
-                children: <Widget>[
-                  cirteria(name: "jardin",icon: Icons.location_on, ispresent: false),
-                  cirteria(name: "jardin",icon: Icons.location_on, ispresent: false),
-                  cirteria(name: "jardin",icon: Icons.location_on, ispresent: false),
-                  cirteria(name: "jardin",icon: Icons.location_on, ispresent: true),
-                  cirteria(name: "jardin",icon: Icons.location_on, ispresent: true),
-                  cirteria(name: "jardin",icon: Icons.location_on, ispresent: true),
-                  cirteria(name: "jardin",icon: Icons.location_on, ispresent: false),
+                Container (
+                  margin: const EdgeInsets.only(top: 5.0),
+                  padding: const EdgeInsets.all(3.0),
+                  child: new Row (
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Column(
+                          children: [
+                            Icon(Icons.kitchen, color: Colors.white),
+                            Text(realEstateData.size.toString() + " m²",
+                                style: TextStyle(color: Colors.white)
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 20),
+                        Column(
+                          children: [
+                            Icon(Icons.room, color: Colors.white),
+                            Text(realEstateData.nbRooms.toString() + " pieces",
+                                style: TextStyle(color: Colors.white)
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 20),
+                        Column(
+                          children: [
+                            Icon(Icons.outdoor_grill, color: Colors.white),
+                            Text(realEstateData.hasGarden.toString() + " Jardin",
+                                style: TextStyle(color: Colors.white)
+                            ),
+                          ],
+                        ),
+                      ]
+                  ),
+                ),
+
+                Container (
+                  margin: const EdgeInsets.only(top: 5.0),
+                  padding: const EdgeInsets.all(3.0),
+                  child: new Row (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(realEstateData.accroche,
+                          style: TextStyle(color: Colors.white)
+                      ),
+                    ],
+                  ),
+                ),
+
+                Container (
+                  margin: const EdgeInsets.only(top: 5.0),
+                  padding: const EdgeInsets.all(3.0),
+                  child: new Row (
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Column(
+                          children: [
+                            Icon(Icons.eco, color: Colors.white),
+                            Text("Classe énergie: " + realEstateData.energyClass,
+                                style: TextStyle(color: Colors.green)
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 20),
+                        Column(
+                          children: [
+                            Icon(Icons.gesture, color: Colors.white),
+                            Text("GES: " + realEstateData.gesClass,
+                                style: TextStyle(color: Colors.green)
+                            ),
+                          ],
+                        ),
+                      ]
+                  ),
+                ),
 
 
-                ]
-            )
-          ],
-        ),
+                Container (
+                  margin: const EdgeInsets.only(top: 5.0),
+                  padding: const EdgeInsets.all(3.0),
+                  child: new Row (
+                    children: <Widget>[
+                      Text(
+                          'Description :',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.white),
+                          textAlign: TextAlign.left
+                      ),
+                    ],
+                  ),
+                ),
 
+                Container (
+                  margin: const EdgeInsets.only(top: 5.0),
+                  padding: const EdgeInsets.all(3.0),
+                  child: new Column (
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      new Text (realEstateData.description,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white)
+                      ),
+                    ],
+                  ),
+                ),
+
+                Container (
+                  margin: const EdgeInsets.only(top: 5.0),
+                  padding: const EdgeInsets.all(3.0),
+                  child: new Row (
+                    children: <Widget>[
+                      Icon(Icons.location_on, color: Colors.white),
+                      Text(realEstateData.address,
+                          style: TextStyle(color: Colors.white)
+                      ),
+                    ],
+                  ),
+                ),
+          ]
+        )
+      )
     );
-  }
-
-  Column cirteria({String name, IconData icon, bool ispresent}) {
-    if(ispresent){
-      return Column(
-          children: <Widget>[
-            Icon(
-              icon, //Icons.location_on,
-              color: ksecondaryColor,
-            ),
-            Text(
-              name,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.green),
-            ),
-
-          ]
-      );
-    }else{
-      return Column(
-
-          children: <Widget>[
-            Icon(
-              icon, //Icons.location_on,
-              color: ksecondaryColor,
-            ),
-            Text(
-              name,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.red),
-            ),
-
-          ]
-      );
-    }
   }
 }
